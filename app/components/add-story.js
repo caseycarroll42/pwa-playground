48009661645f747b5a27d0308532c26f9cb91730
caseycarroll42 <aiyakc@gmail.com>
2017-05-08 12:47:38 -0500

    saving for later, trying new direction...

--- app/components/add-story.js
+++ app/components/add-story.js
@@ -4,6 +4,15 @@ class AddStory extends HTMLElement {
 	constructor() {
 		super()
 		console.log("add story is alive")
+		this.shadow = this.attachShadow({mode: "open"})
+		this.shadow.appendChild(this.storyForm())
+	}
+
+	storyForm() {
+		let storyForm = document.createElement("form")
+		storyForm.id = "new-story-form"
+		storyForm.action = "add-story"
+		return storyForm
 	}
 }
 
